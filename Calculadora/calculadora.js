d7e8427f80e9bf6097ae30cf40b35052ef7b6e7a
jhonattansousa <jhonatann95@hotmail.com>
2020-11-01 17:50:12 -0300

    My WebSite Finalizando

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,3 +1,61 @@
+let inputResultado = document.getElementById('resultado')    
+
+let calculo = {
+    primeiroValor : 0,
+    segundoValor : 0,
+    funcaoParaCalcular: null,
+};
+
+// adicionar evento para iniciar quando a janela carregar
+window.addEventListener("load", function (){
+    atribuirEventos();
+})
+
+// Atribuir eventos para os botoões da calculadora
+function atribuirEventos(){
+    
+    documento.getElementById("clear").addEventListener("onclick", Apagar)
+}
+
+const soma = (x,y) => {
+    return x + y;
+}  
+
+const sub = (x,y) => {
+    return x - y;
+} 
+
+const mult = (x,y) => {
+    return x * y;
+} 
+
+const div = (x,y) => {
+    if(y == 0){
+        return "Erro, divisão por zero";
+    }
+        return x / y
+} 
+
+const calculadora = (x,y,operacao) => {
+    return operacao(x,y)
+}
+
+function limparDados(){
+   inputResultado.value = "";
+
+   calculo.primeiroValor = 0;
+   calculo.segundoValor = 0;
+   calculo.funcaoParaCalcular= null;
+
+}
+
+function atribuirOperacao(operador){
+    if(operador == "+"){
+        calculo.funcaoParaCalcular = soma;
+    }
+}
+
+
 /*function somar () {
     let tn1 = window.document.getElementById('entrada-1')
     let tn2 = window.document.getElementById('entrada-2')
@@ -37,28 +95,3 @@ function dividir () {
     let s = n1 / n2
     res.innerHTML = `<strong> ${s} </strong>`
     }*/
-
-const soma = (x,y) => {
-    return x+y
-}  
-
-const sub = (x,y) => {
-    return x-y
-} 
-
-const mult = (x,y) => {
-    return x*y
-} 
-
-const div = (x,y) => {
-    return x/y
-} 
-
-const calculadora = (x,y,operacao) => {
-    return operacao(x,y)
-}
-
-// calculadora(10,2,soma) //
-//const resultado = calculadora(x,y,operacao) //
-
-    
